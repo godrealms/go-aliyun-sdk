9d07ed77cd6cf713b882ce8440083c988d2bd94b
wujie <wujie812@qq.com>
2026-04-20 19:33:32 +0800

    feat(trade): add AlipayTradeRoyaltyRelationBind (alipay.trade.royalty.relation.bind)

---
+++ alipay/alipay.trade.royalty.relation.bind.go
@@ -0,0 +1,39 @@
+package alipay
+
+import (
+	"context"
+	"time"
+
+	"github.com/godrealms/go-aliyun-sdk/alipay/types"
+	"github.com/godrealms/go-aliyun-sdk/community"
+)
+
+// AlipayTradeRoyaltyRelationBind 分账关系绑定（alipay.trade.royalty.relation.bind）
+func (c *Client) AlipayTradeRoyaltyRelationBind(req *types.TradeRoyaltyRelationBind) (*types.AlipayTradeRoyaltyRelationBindResponse, error) {
+	data := types.PublicRequestParameters{
+		AppId:        c.AppId,
+		Method:       "alipay.trade.royalty.relation.bind",
+		Format:       "JSON",
+		Charset:      "UTF-8",
+		SignType:     "RSA2",
+		Timestamp:    time.Now().Format("2006-01-02 15:04:05"),
+		Version:      "1.0",
+		AppAuthToken: c.AppAuthToken,
+		BizContent:   req.ToString(),
+	}
+	signature, err := community.NewSignatureHelper(c.PrivateKey)
+	if err != nil {
+		return nil, err
+	}
+	data.Sign, err = signature.GenerateSignature(data)
+	if err != nil {
+		return nil, err
+	}
+	value := data.ToUrlValue()
+	result := &types.AlipayTradeRoyaltyRelationBindResponse{}
+	err = c.Http.Get(context.Background(), "", value, result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
---
+++ example/alipay/alipay.trade.royalty.relation.bind.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/godrealms/go-aliyun-sdk/alipay"
+	"github.com/godrealms/go-aliyun-sdk/alipay/types"
+)
+
+func main() {
+	client := alipay.NewClient()
+	client.AppId = os.Getenv("ALIPAY_APP_ID")
+	client.PrivateKey = os.Getenv("ALIPAY_PRIVATE_KEY")
+	client.AlipayPublicKey = os.Getenv("ALIPAY_PUBLIC_KEY_FROM_ALIPAY")
+
+	resp, err := client.AlipayTradeRoyaltyRelationBind(&types.TradeRoyaltyRelationBind{
+		OutRequestNo: os.Getenv("ALIPAY_OUT_REQUEST_NO"),
+		ReceiverList: os.Getenv("ALIPAY_RECEIVER_LIST"),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Code: %s", resp.Response.Code)
+}
