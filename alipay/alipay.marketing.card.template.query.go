d295506faab06512e2c2e05f71a719edd976a19d
wujie <wujie812@qq.com>
2026-04-20 18:02:48 +0800

    feat(marketing): 新增 alipay.marketing.card.template.query 查询会员卡模板接口

---
+++ alipay/alipay.marketing.card.template.query.go
@@ -0,0 +1,38 @@
+package alipay
+
+import (
+	"context"
+	"time"
+
+	"github.com/godrealms/go-aliyun-sdk/alipay/types"
+	"github.com/godrealms/go-aliyun-sdk/community"
+)
+
+// AlipayMarketingCardTemplateQuery 查询会员卡模板（alipay.marketing.card.template.query）
+func (c *Client) AlipayMarketingCardTemplateQuery(request *types.CardTemplateQuery) (*types.AlipayMarketingCardTemplateQueryResponse, error) {
+	data := types.PublicRequestParameters{
+		AppId:        c.AppId,
+		Method:       "alipay.marketing.card.template.query",
+		Format:       "JSON",
+		Charset:      "UTF-8",
+		SignType:     "RSA2",
+		Timestamp:    time.Now().Format("2006-01-02 15:04:05"),
+		Version:      "1.0",
+		AppAuthToken: c.AppAuthToken,
+		BizContent:   request.ToString(),
+	}
+	signature, err := community.NewSignatureHelper(c.PrivateKey)
+	if err != nil {
+		return nil, err
+	}
+	data.Sign, err = signature.GenerateSignature(data)
+	if err != nil {
+		return nil, err
+	}
+	result := &types.AlipayMarketingCardTemplateQueryResponse{}
+	err = c.Http.PostForm(context.Background(), "", data.ToUrlValue(), nil, result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
---
+++ alipay/types/alipay.marketing.card.template.query.go
@@ -0,0 +1,27 @@
+package types
+
+import "encoding/json"
+
+type CardTemplateQuery struct {
+	//【描述】卡模板 ID，由 AlipayMarketingCardTemplateCreate 返回
+	//【示例值】TPL20260420001
+	TemplateId string `json:"template_id"`
+}
+
+func (r *CardTemplateQuery) ToString() string {
+	marshal, _ := json.Marshal(r)
+	return string(marshal)
+}
+
+type CardTemplateQueryDetail struct {
+	PublicResponseParameters
+	TemplateId   string `json:"template_id"`
+	TemplateName string `json:"template_name"`
+	//【描述】模板状态：NORMAL | STOP
+	Status string `json:"status"`
+}
+
+type AlipayMarketingCardTemplateQueryResponse struct {
+	AlipayMarketingCardTemplateQueryResponse CardTemplateQueryDetail `json:"alipay_marketing_card_template_query_response"`
+	Sign                                      string                  `json:"sign"`
+}
