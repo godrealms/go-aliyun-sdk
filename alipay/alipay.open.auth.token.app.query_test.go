e1cdd334a3f7ac7991c7c292652afe56b961e9f4
wujie <wujie812@qq.com>
2026-04-20 10:56:50 +0800

    feat(isv): 新增 alipay.open.auth.token.app.query 接口

---
+++ alipay/alipay.open.auth.token.app.query_test.go
@@ -0,0 +1,24 @@
+package alipay
+
+import (
+	"testing"
+
+	"github.com/godrealms/go-aliyun-sdk/alipay/types"
+)
+
+func TestAlipayOpenAuthTokenAppQuery(t *testing.T) {
+	mockResp := `{"alipay_open_auth_token_app_query_response":{"code":"10000","msg":"Success","user_id":"2088xxx","auth_app_id":"2021xxx","app_auth_token":"existtoken","app_refresh_token":"refreshtoken","expires_in":31536000,"re_expires_in":32140800,"token_begin_time":"2026-04-20 10:00:00","status":"NORMAL"},"sign":"fakesign"}`
+
+	client, _ := newTestClient(t, mockResp)
+
+	req := &types.OpenAuthTokenAppQuery{
+		AppAuthToken: "existtoken",
+	}
+	resp, err := client.AlipayOpenAuthTokenAppQuery(req)
+	if err != nil {
+		t.Fatalf("AlipayOpenAuthTokenAppQuery error: %v", err)
+	}
+	if resp.AlipayOpenAuthTokenAppQueryResponse.Status != "NORMAL" {
+		t.Errorf("Status = %q, want NORMAL", resp.AlipayOpenAuthTokenAppQueryResponse.Status)
+	}
+}
